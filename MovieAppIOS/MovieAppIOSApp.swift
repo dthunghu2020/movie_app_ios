aa55c6a7c8be4f0d79a354df00985872eb51157a
hungdt <hungdt@newwave.vn>
2023-02-20 15:50:24 +0700

    create project

---
+++ MovieAppIOS/MovieAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppIOSApp.swift
+//  MovieAppIOS
+//
+//  Created by hungdt on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
